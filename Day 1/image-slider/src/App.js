d6be6863e3291b2c74ad67e1a4b009001f26e247
Adel <ahmeddadel9988@gmail.com>
2024-05-17 20:11:31 +0300

    Image Slider

---
+++ Day 1/image-slider/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import './Components/image-slider.jsx'
+import ImageSlider from './Components/image-slider.jsx';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Slider</h1>
+       <ImageSlider/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
